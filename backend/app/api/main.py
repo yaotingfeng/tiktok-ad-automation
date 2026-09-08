d886c7f69dbc3062baad11e6cc3a01514b4c6df9
yaotingfeng <384675592@qq.com>
2026-09-09 05:02:23 +0800

    materials: register object upload and library endpoints

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.routes import integrations, login, users, utils
 from app.modules.accounts.router import router as accounts_router
+from app.modules.materials.router import router as materials_router
 from app.modules.providers.router import router as providers_router
 from app.modules.strategies.api import router as strategies_router
 from app.modules.tenants.router import router as tenants_router
@@ -13,5 +14,6 @@ api_router.include_router(utils.router)
 api_router.include_router(integrations.router)
 api_router.include_router(tenants_router)
 api_router.include_router(accounts_router)
+api_router.include_router(materials_router)
 api_router.include_router(providers_router)
 api_router.include_router(strategies_router)
